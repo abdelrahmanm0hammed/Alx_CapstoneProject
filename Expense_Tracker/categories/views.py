bb48f1ff9833df223a8dc340ddefa528b05e5004
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-12 16:18:04 +0200

    creating Category views that implement crud operation on category model

--- Expense_Tracker/categories/views.py
+++ Expense_Tracker/categories/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset= Category.objects.all()
+    serializer_class=CategorySerializer
+
+
+class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= Category.objects.all()
+    serializer_class=CategorySerializer
+    permission_classes= [IsAuthenticatedOrReadOnly]
