9f36b8fa4180a172accce5b8d5a265275be4d7e3
AbdelrahmanMohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-13 11:59:28 +0200

    updated expense serializer to show the category field in the browser by its name

--- Expense_Tracker/expenses/serializers.py
+++ Expense_Tracker/expenses/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Expense
-
+from categories.models import Category
 
 class ExpenseSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    category = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Category.objects.all()
+    )
     class Meta:
         model = Expense
         fields = "__all__"
