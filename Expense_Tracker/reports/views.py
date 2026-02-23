c52339d9d545307ff24e7a3e6c224fd3fbefb013
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-23 15:16:44 +0200

    monthly report view fixed and tested'

--- Expense_Tracker/reports/views.py
+++ Expense_Tracker/reports/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.db.models import Sum
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,45 +5,33 @@ from rest_framework.permissions import IsAuthenticated
 from expenses.models import Expense
 from incomes.models import Income
 
-
 class MonthlyReportView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        print(request.query_params)
         year = request.query_params.get("year")
         month = request.query_params.get("month")
 
-        if  year is None or month is None:
-            return Response(
-                {"error":"Please provide year and month"},status=400
-            )
-
-        
+        if year is None or month is None:
+            return Response({"error":"Please provide year and month"}, status=400)
         year = int(year)
         month = int(month)
-        
-        
 
-        total_expense = (Expense.objects.filter(
-            user=request.user,
-            date__year =year,
-            date__month = month
-        ).aaggregate(total=Sum("amount")).get("total") or 0)
-
-        total_income = (Income.objects.filter(
-            user =request.user,
-            date__year =year,
-            date__month = month,).aaggregate(total=Sum("amount")).get("total") or 0)
-        
-        
-        net_balance = total_income - total_expense
-
+        total_expense = (
+            Expense.objects.filter(user=request.user, date__year=year, date__month=month)
+            .aggregate(total=Sum("amount"))
+            .get("total") or 0
+        )
+        total_income = (
+            Income.objects.filter(user=request.user, date__year=year, date__month=month)
+            .aggregate(total=Sum("amount"))
+            .get("total") or 0
+        )
+       
         return Response({
-            "month":f"{year}-{month}",
             "total_income":total_income,
             "total_expense":total_expense,
-            "net_balance":net_balance,
-
+            "net_balance": total_income - total_expense
+            
         })
-        
+       
