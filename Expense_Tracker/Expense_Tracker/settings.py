e1cc9e5fd4e58bfa69b649b3ea077862f726cf92
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-24 17:37:01 +0200

    adding filters to settings :

--- Expense_Tracker/Expense_Tracker/settings.py
+++ Expense_Tracker/Expense_Tracker/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'incomes',
     'reports',
+    'django-filters',
 
 ]
 
@@ -131,3 +132,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+}
