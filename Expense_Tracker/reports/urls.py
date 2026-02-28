1b28ff06c936a4d8098214c0315c88e96b7d810d
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-02-28 23:16:11 +0200

    creating yearly report for total_income, total_expense and net_balance

--- Expense_Tracker/reports/urls.py
+++ Expense_Tracker/reports/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("monthly/", views.MonthlyReportView.as_view(), name="monthly-report"),
-    path("monthly/export/",views.MonthlyReportCSVExportView.as_view(), name="monthly-report-export")
+    path("monthly/export/",views.MonthlyReportCSVExportView.as_view(), name="monthly-report-export"),
+    path("yearly/",views.YearlyReportView.as_view(), name='yearly-report')
 ]
--- Expense_Tracker/reports/views.py
+++ Expense_Tracker/reports/views.py
@@ -6,6 +6,8 @@ from expenses.models import Expense
 from incomes.models import Income
 import csv
 from django.http import HttpResponse
+from django.db.models.functions import ExtractMonth
+
 
 class MonthlyReportView(APIView):
     permission_classes = [IsAuthenticated]
@@ -84,3 +86,56 @@ class MonthlyReportCSVExportView(APIView):
                 expense.description
             ])
         return response
+
+class YearlyReportView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        year = request.query_params.get("year")
+
+        if not year:
+            return Response({"error": "Year is required"}, status=400)
+        try:
+            year = int(year)
+        except ValueError:
+            return Response({"error": "Year must be integer "}, status=400)
+        
+        # Group Expenses by month
+        expense_data = (
+            Expense.objects.filter(user=request.user, date__year=year)
+            .annotate(month=ExtractMonth("date"))
+            .values("month")
+            .annotate(total_expense=Sum("amount"))
+            .order_by("month")
+        )
+        # Group Income by month 
+        income_data = (
+            Income.objects.filter(user=request.user, date__year=year)
+            .annotate(month=ExtractMonth("date"))
+            .values("month")
+            .annotate(total_income=Sum("amount"))
+            .order_by("month")
+        )
+        # convert querysets into dictionaries for easier merging
+        expense_dict = {item["month"]: item["total_expense"] for item in expense_data}
+        income_dict = {item["month"]: item["total_income"] for item in income_data}
+
+        # Get all months that appear in either income or expense
+        all_month = sorted(set(expense_dict.keys()) | set(income_dict.keys()))
+
+        monthly_data = []
+
+        for month in all_month:
+            total_income = income_dict.get(month, 0)
+            total_expense = expense_dict.get(month, 0)
+
+            monthly_data.append({
+                "month":month,
+                "total_income": total_income,
+                "total_expense": total_expense,
+                "net_balance": total_income - total_expense
+            })
+        return Response({
+            "year": year,
+            "monthly_data":monthly_data
+        })
