3a53e00fa585e59c5415b18832e855a403227eed
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-03-02 23:55:15 +0200

    created url for token obtain and token refresh based on jwt auth

--- Expense_Tracker/Expense_Tracker/urls.py
+++ Expense_Tracker/Expense_Tracker/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path("expenses/", include('expenses.urls')),
     path("incomes/", include('incomes.urls')),
     path("reports/",include('reports.urls')),
-    path
+    path("api/token/",TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh",TokenRefreshView.as_view(), name="token_refresh"),
 ]
