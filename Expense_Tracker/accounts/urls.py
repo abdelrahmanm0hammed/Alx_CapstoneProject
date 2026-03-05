8836e968be707426cd8a229187754ebb9ab20282
abdelrahmanm0hammed <abdelrahmanmuhammed04@gmail.com>
2026-03-05 23:18:18 +0200

    creating a blacklisting token logout

--- Expense_Tracker/accounts/urls.py
+++ Expense_Tracker/accounts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
 
     path("register/",views.register,name='register'),
     path("login/",views.login_view,name='login'),
     path("logout/",views.logout, name="logout"),
+    path("api/logout/",views.LogoutView.as_view(), name='token-logout')
 ]
